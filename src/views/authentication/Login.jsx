571fd50448a7b31aa574ad5a7f4be8037973a4f9
prabhu797 <prabhudesai007@gmail.com>
2024-11-28 16:05:10 +0530

    some changes

--- src/views/authentication/Login.jsx
+++ src/views/authentication/Login.jsx
@@ -4,7 +4,7 @@ export default function Login() {
 
     const frappeServer = "novelhouston.com";
     const clientId = "2l9s5n1pj1";
-    const redirectURL = "http://10.80.3.70:8080/loggedin";
+    const redirectURL = "https://prabhu797.github.io/dashboard/loggedin";
 
     useEffect(() => {
         window.location.href = `https://${frappeServer}/api/method/frappe.integrations.oauth2.authorize?client_id=${clientId}&redirect_uri=${redirectURL}&response_type=code`;
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import svgr from '@svgr/rollup';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/teamconnect/",
   server: {
 		port: 8080,
 	},
