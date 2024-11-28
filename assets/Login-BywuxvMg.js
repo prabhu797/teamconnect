4a74389910ff6fb129102afa45e59d13b70a36cf
prabhu797 <prabhudesai007@gmail.com>
2024-11-28 15:54:29 +0530

    msg

---
+++ assets/Login-BywuxvMg.js
@@ -0,0 +1,1 @@
+import{r}from"./index-Dd1Qg3kd.js";function i(){const t="novelhouston.com",e="2l9s5n1pj1",o="http://10.80.3.70:8080/loggedin";return r.useEffect(()=>{window.location.href=`https://${t}/api/method/frappe.integrations.oauth2.authorize?client_id=${e}&redirect_uri=${o}&response_type=code`},[]),"Login"}export{i as default};
