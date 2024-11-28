06070e3f343f846ff4d5a7210b791bac610fce3a
prabhu797 <prabhudesai007@gmail.com>
2024-11-28 15:57:29 +0530

    gh pages update

--- assets/Logout-BxICDQ1c.js
+++
@@ -1,1 +0,0 @@
-import{Z as t,r as o,j as a}from"./index-Dd1Qg3kd.js";function r(){const e=t();return o.useEffect(()=>{localStorage.removeItem("access_token"),setTimeout(()=>{e("/dashboard")},1e3)},[]),a.jsx("div",{children:"Logging out..."})}export{r as default};
